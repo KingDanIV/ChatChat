79484489ed01b74f8e3508b28bb7cbb11f4d99fc
Daniel Earl <danearl4@gmail.com>
2019-03-12 14:59:13 +0000

    Initial Commit

---
+++ ChatChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatChat
+//
+//  Created by Daniel Earl on 12/03/2019.
+//  Copyright © 2019 Daniel Earl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
